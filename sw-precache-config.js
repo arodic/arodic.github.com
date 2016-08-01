31bab19957faa77b817eaa9ffb7dd8c65e5e1987
Aki Rodic <aleksandar.xyz@gmail.com>
2016-08-01 10:39:42 +0200

    fidling with travis.

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -1,7 +1,17 @@
 module.exports = {
   staticFileGlobs: [
-    '/index.html',
     '/manifest.json',
+    '/index.html',
+    '/src/page/about.html',
+    '/src/page/projects.html',
+    '/src/page/project/cardboard-launch.html',
+    '/src/page/project/daily-routines.html',
+    '/src/page/project/flux-factory.html',
+    '/src/page/project/google-zeitgeist-2013.html',
+    '/src/page/project/just-a-reflector.html',
+    '/src/page/project/star-wars-1313.html',
+    '/src/page/project/unnumbered-sparks.html',
+    '/src/page/project/webgl-jellyfish.html',
     '/bower_components/webcomponentsjs/webcomponents-lite.min.js'
   ],
   navigateFallback: '/index.html'
--- travis-runner.sh
+++ travis-runner.sh
@@ -1,15 +1,29 @@
 #!/bin/bash -e
 set -o pipefail
 
-if [ "$TRAVIS_BRANCH" = "dev" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]  && [ "$TRAVIS_NODE_VERSION" = "5.1" ]
+if [ "$TRAVIS_BRANCH" = "$SRC_BRANCH" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]  && [ "$TRAVIS_NODE_VERSION" = "5.1" ]
 then
   git config --global user.email "aleksandar.xyz@gmail.com"
-  git config --global user.name "auto deployer"
+  git config --global user.name "Travis-CI"
 
   build_polymer () {
     echo Bulding with polymer-cli
     polymer build
   }
 
+  # deploy_to_gh () {
+  #   echo Deploying to GitHub
+  #   rm -rf ../out || exit 0;
+  #   cp -r build/bundled ../out
+  #   git fetch ${DEST_BRANCH}
+  #   git checkout ${DEST_BRANCH}
+  #   rm -rf *
+  #   cp -r ../out/* .
+  #   git add .
+  #   git commit -m "Travis-CI: Deployed to Github"
+  #   git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:master > /dev/null 2>&1
+  # }
+
   build_polymer
+  # deploy_to_gh
 fi
