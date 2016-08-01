8eadad324031b9345b245f6425ac570d12e96a39
Aki Rodic <aleksandar.xyz@gmail.com>
2016-08-01 14:46:51 +0200

    Layour fix, manifest update.

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -1,9 +1,11 @@
 module.exports = {
   staticFileGlobs: [
     '/manifest.json',
+    '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
     '/index.html',
     '/src/page/about.html',
     '/src/page/projects.html',
+    // projects
     '/src/page/project/cardboard-launch.html',
     '/src/page/project/daily-routines.html',
     '/src/page/project/flux-factory.html',
@@ -12,7 +14,15 @@ module.exports = {
     '/src/page/project/star-wars-1313.html',
     '/src/page/project/unnumbered-sparks.html',
     '/src/page/project/webgl-jellyfish.html',
-    '/bower_components/webcomponentsjs/webcomponents-lite.min.js'
+    // images
+    'images/project/cardboard-launch.jpg',
+    'images/project/daily-routines.jpg',
+    'images/project/flux-factory.jpg',
+    'images/project/google-zeitgeist-2013.jpg',
+    'images/project/just-a-reflector.jpg',
+    'images/project/star-wars-1313.jpg',
+    'images/project/unnumbered-sparks.jpg',
+    'images/project/webgl-jellyfish.jpg'
   ],
   navigateFallback: '/index.html'
 };
