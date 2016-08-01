435c360047cac9e09f9db2e88dc476c781041856
Aki Rodic <aleksandar.xyz@gmail.com>
2016-08-01 10:31:43 +0200

    Travis update

--- travis-runner.sh
+++ travis-runner.sh
@@ -12,7 +12,4 @@ then
   }
 
   build_polymer
-
-elif [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]  && [ "$TRAVIS_NODE_VERSION" != "5.1" ]
-then
-  echo "Do Nothing, only deploy with Node 5.1"
+fi
