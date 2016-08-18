70003a21777b2371fe839c210918da40c4fe3daa
Aki Rodic <aleksandar.xyz@gmail.com>
2016-08-18 11:53:06 +0200

    updated travis for firebase.

--- travis-runner.sh
+++ travis-runner.sh
@@ -1,36 +1,22 @@
 #!/bin/bash -e
-# set -o pipefail
-set -o errexit
+set -o pipefail
+# set -o errexit
 
-if [ "$TRAVIS_BRANCH" = "dev" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]  && [ "$TRAVIS_NODE_VERSION" = "5.1" ]
+if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]  && [ "$TRAVIS_NODE_VERSION" = "5.1" ]
 then
   git config --global user.email "aleksandar.xyz@gmail.com"
   git config --global user.name "Travis-CI"
 
   build_polymer () {
     echo Bulding with polymer-cli
-    polymer build
+    polymer build --sources  images/**/*
   }
 
-  # deploy_github () {
-  #   echo Deploying to GitHub
-  #   git config --global user.email "nobody@nobody.org"
-  #   git config --global user.name "Travis CI"
-  #
-  #   cd ..
-  #   git clone "https://${GITHUB_TOKEN}@${GH_REF}" deploy
-  #   cd deploy
-  #   ls
-  #   # rm -rf ../bundled
-  #   # cp -r build/bundled ../
-  #   # cd ../bundled
-  #   #
-  #   # git init
-  #   # git add .
-  #   # git commit -m "Deploy to Github Pages"
-  #   # git push -f -q "https://${GITHUB_TOKEN}@${GH_REF}" dev:master > /dev/null 2>&1
-  # }
+  deploy_firebase () {
+    echo Deploying to Firebase
+    firebase deploy --token "$FIREBASE_TOKEN" -m "Auto Deployed by Travis CI"
+  }
 
   build_polymer
-  # deploy_github
+  deploy_firebase
 fi
